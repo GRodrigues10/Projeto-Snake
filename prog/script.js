776723f4b4ee9a0ce3f1a645215c2ae91a53f4cd
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-06-30 13:46:30 -0300

    atualização

--- prog/script.js
+++ prog/script.js
@@ -1,18 +1,47 @@
 const canvas = document.querySelector('canvas');
 
-// O CONSTEXTO É IMPORTANTE DIZER SEMPRE.
+// O CONTEXTO É IMPORTANTE DIZER SEMPRE.
 const ctx = canvas.getContext('2d');
 
 // tamnho da combrinha(cada quadrado)
 const size = 30;
 
 const snake = [
-    {x: 200, y: 200 },
-    {x: 230, y: 200 },
+    {x: 270, y: 240 },
+    
 ];
+
+const randomNumber = (min, max)=>{
+    return Math.round(Math.random() * (max - min) + min);
+}
+
+const randomPosition = ()=>{
+    const number = randomNumber(0, canvas.width - size);
+    return Math.round(number / 30) * 30;
+}
+
+const food = {
+    x:randomPosition(),
+    y:randomPosition(),
+    color:'yellow'
+}
 
 let direction;
 let loopId;
+
+const drawFood = ()=>{
+
+    const {x, y, color} = food
+
+    ctx.shadowColor = color
+    ctx.shadowBlur = 50;
+    ctx.fillStyle = color
+    ctx.fillRect(x, y, size, size);
+    ctx.shadowBlur = 0;
+
+}
+
+
 
 const drawSnake = () => {
     ctx.fillStyle = '#ddd'
@@ -53,9 +82,34 @@ const moveSnake = ()=>{
     snake.shift();
 }
 
+const drawGrid = ()=>{
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = '#0c1022';
+
+    for(let i = 30; i < canvas.width; i += 30){
+        ctx.beginPath()
+        ctx.lineTo(i, 0);
+        ctx.lineTo(i, 600);
+        ctx.stroke();
+
+        ctx.beginPath()
+        ctx.lineTo(0, i);
+        ctx.lineTo(600, i);
+        ctx.stroke()
+    
+    }
+
+   
+  
+}
+
+
+
 const gameLoop = () =>{
     clearInterval(loopId);
     ctx.clearRect(0 ,0, 600, 600);
+    drawGrid();
+    drawFood();
     moveSnake()
     drawSnake();
 
@@ -64,7 +118,22 @@ const gameLoop = () =>{
     },300);
 }
 
-gameLoop()
+gameLoop();
+
+document.addEventListener('keydown', ({key}) =>{
+    if(key == 'ArrowRight' && direction != 'left'){
+        direction = 'right'
+    }
+    if(key == 'ArrowLeft' && direction != 'right'){
+        direction = 'left'
+    }
+    if(key == 'ArrowUp' && direction != 'down'){
+        direction = 'up'
+    }
+    if(key == 'ArrowDown' && direction != 'up'){
+        direction = 'down'
+    }
+})
 
 
 
